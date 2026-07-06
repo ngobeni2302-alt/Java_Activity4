4961bd0b33103b3ff04644e45396fdfe821f983f
Ntsako <Ngobeni2302@gmail.com>
2026-07-06 02:00:54 +0200

    Order.java completion

--- src/main/java/com/shop/model/Order.java
+++ src/main/java/com/shop/model/Order.java
@@ -1,5 +1,18 @@
 package com.shop.model;
 
 public class Order {
+    private String orderId;
+    private Item item;
+    private String shippingMethod;
+
+    public Order(String orderId, Item item, String shippingMethod){
+        this.orderId = orderId;
+        this.item = item;
+        this.shippingMethod = shippingMethod;
+    }
+
+    public String getOrderId(){return this.orderId;}
+    public Item getItem(){return this.item;}
+    public String getShippingMethod(){return this.shippingMethod;}
 
 }
