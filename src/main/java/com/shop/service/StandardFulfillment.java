8f1ff91e560a31ad0ef1b4f3dee18c52984aef69
ntngoeec025 <ntngoeec025@student.wethinkcode.co.za>
2026-07-07 09:59:29 +0200

    StandardFuilfillment.java completion

--- src/main/java/com/shop/service/StandardFulfillment.java
+++ src/main/java/com/shop/service/StandardFulfillment.java
@@ -1,7 +1,14 @@
 package com.shop.service;
 
+import com.shop.model.Order;
+import com.shop.model.Shipment;
+
 public class StandardFulfillment extends FulfillmentService {
 
-
+    @Override
+    public Shipment processOrder(Order order){
+        double newCost = order.getItem().getPrice() + 5.00;
+        return new Shipment(newCost, "Shipped via "+ order.getShippingMethod());
+    }
 
 }
